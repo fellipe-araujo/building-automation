7a6f953ca6af43f6c096ddd546108f7fb9f99c4a
Fellipe <araujo.fellipe@hotmail.com>
2021-09-27 20:47:09 -0300

    Created threads and finished first version of the distributed server.

---
+++ DistributedServer/inc/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#include "socket_tcp.h"
+#include "socket_quit.h"
+#include "gpio.h"
+#include "dht22.h"
+
+#endif
--- DistributedServer/src/main.c
+++ DistributedServer/src/main.c
@@ -1,18 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "dht22.h"
+#include "main.h"
+
+void ground_floor() {
+  gpio_handler(0);
+}
+
+void first_floor() {
+  gpio_handler(1);
+}
 
 int main () {
-  float temperature = -1;
-  float humidity  = -1;
-
+  gpio_setup();
+  quit_setup();
   dht22_setup();
 
-  while (1) {
-    dht22_read_data(&temperature, &humidity);
-    sleep(1);
-  }
+  pthread_t tid[3];
+
+  pthread_create(&tid[0], NULL, (void *)server_handler, (void *)NULL);
+  pthread_create(&tid[1], NULL, (void *)ground_floor, (void *)NULL);
+  pthread_create(&tid[2], NULL, (void *)first_floor, (void *)NULL);
+
+  pthread_join(tid[0], NULL);
+	pthread_join(tid[1], NULL);
+	pthread_join(tid[2], NULL);
 
   return 0;
 }
