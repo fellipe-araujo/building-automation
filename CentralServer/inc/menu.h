6ecb9ba327cc31ec80224e11fc17d7369c7bc1be
Fellipe <araujo.fellipe@hotmail.com>
2021-09-28 19:20:54 -0300

    Created function that prints devices_out data.

--- CentralServer/inc/menu.h
+++ CentralServer/inc/menu.h
@@ -4,5 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include "data.h"
+
+void print_menu(WINDOW *window, int highlight);
+void clear_menu(WINDOW *window_param);
+void print_data(Data data);
 
 #endif
--- CentralServer/src/menu.c
+++ CentralServer/src/menu.c
@@ -1,4 +1,5 @@
 #include "menu.h"
+#include "data.h"
 
 #define WIDTH 94
 #define HEIGHT 28
@@ -55,4 +56,76 @@ void clear_menu(WINDOW *window_param) {
 	wborder(window_param, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');  
 	wrefresh(window_param);
 	delwin(window_param); 
+}
+
+void print_data(Data data) {
+  wattron(window, COLOR_PAIR(3));
+	mvwprintw(window, 4, 41, data.dev_out.ls_t01 == 1 ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(3));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 5, 41, data.dev_out.ls_t02 ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 6, 41, data.dev_out.lc_t ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 7, 41, data.dev_out.ac_t ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 8, 41, data.dev_out.asp ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 9, 41, data.dev_out.ls_101 ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 10, 41, data.dev_out.ls_102 ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 11, 41, data.dev_out.lc_1 ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 12, 41, data.dev_out.alarm ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 13, 41, data.dev_out.alarm_playing ? "ON " : "OFF");
+	wattroff(window, COLOR_PAIR(2));
+
+  mvwprintw(window, 19, 2, "Temperatura - Térreo: ");
+  wattron(window, COLOR_PAIR(1));
+  mvwprintw(window, 19, 25, "%4.2f", data.dht22.ground_floor_temperature);
+	wattroff(window, COLOR_PAIR(1));
+  mvwprintw(window, 20, 2, "Umidade     - Térreo: ");
+	wattron(window, COLOR_PAIR(1));
+  mvwprintw(window, 20, 25, "%4.2f", data.dht22.ground_floor_humidity);
+	wattroff(window, COLOR_PAIR(1));
+
+  mvwprintw(window, 19, 40, "Temperatura - Primeiro Andar: ");
+  wattron(window, COLOR_PAIR(1));
+  mvwprintw(window, 19, 70, "%4.2f", data.dht22.first_floor_temperature);
+	wattroff(window, COLOR_PAIR(1));
+  mvwprintw(window, 20, 40, "Umidade     - Primeiro Andar: ");
+	wattron(window, COLOR_PAIR(1));
+  mvwprintw(window, 20, 70, "%4.2f", data.dht22.first_floor_humidity);
+	wattroff(window, COLOR_PAIR(1));
+
+	mvwprintw(window, 25, 2, "Alarme Tocando: ");
+  wattron(window, COLOR_PAIR(2));
+	mvwprintw(window, 25, 18, "%s", data.dev_out.alarm_playing ? "SIM" : "NAO");
+	wattroff(window, COLOR_PAIR(2));
+
+	mvwprintw(window, 25, 50, "Pessoas: ");
+  wattron(window, COLOR_PAIR(1));
+	mvwprintw(window, 25, 59, "%s", "0");
+	wattroff(window, COLOR_PAIR(1));
+
+  wrefresh(window);
 }
