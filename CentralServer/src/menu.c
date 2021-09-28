3d734883f2fd44f57b4c256a4a022db685e46c36
Fellipe <araujo.fellipe@hotmail.com>
2021-09-28 18:50:34 -0300

    Added functions to print and clear the menu.

--- CentralServer/src/menu.c
+++ CentralServer/src/menu.c
@@ -18,3 +18,41 @@ char *choices[] = {
   "Ativar o Alarme:",
   "Sair",
 };
+
+int startx = 0;
+int starty = 0;
+int n_choices = sizeof(choices) / sizeof(char *);
+
+void print_menu(WINDOW *window, int highlight) {
+	int x = 2, y = 4, i;
+	box(window, 0, 0);
+
+	for(i = 0; i < n_choices; i++) {
+		if (i == n_choices - 1) {
+			wattron(window, COLOR_PAIR(2));
+		}
+
+		if (highlight == i + 1) {
+			wattron(window, A_REVERSE);
+			mvwprintw(window, y, x, "%s", choices[i]);
+			wattroff(window, A_REVERSE);
+		} else {
+			mvwprintw(window, y, x, "%s", choices[i]);
+		}
+
+		if (i == n_choices - 1) {
+			wattroff(window, COLOR_PAIR(2));
+		}
+
+		++y;
+	}
+
+	wrefresh(window);
+}
+
+void clear_menu(WINDOW *window_param) {
+	wclear(window_param);
+	wborder(window_param, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');  
+	wrefresh(window_param);
+	delwin(window_param); 
+}
