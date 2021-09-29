8a5b84c23e47155318e2c89737ed9cd993ad930a
Fellipe <araujo.fellipe@hotmail.com>
2021-09-29 15:00:02 -0300

    Created two distributed servers: ground floor and first floor.

--- DistributedServer/src/main.c
+++ DistributedServer/src/main.c
@@ -2,10 +2,12 @@
 
 void ground_floor() {
   gpio_handler(0);
+  server_handler(0);
 }
 
 void first_floor() {
   gpio_handler(1);
+  server_handler(1);
 }
 
 int main () {
@@ -13,15 +15,13 @@ int main () {
   quit_setup();
   dht22_setup();
 
-  pthread_t tid[3];
+  pthread_t tid[2];
 
-  pthread_create(&tid[0], NULL, (void *)server_handler, (void *)NULL);
-  pthread_create(&tid[1], NULL, (void *)ground_floor, (void *)NULL);
-  pthread_create(&tid[2], NULL, (void *)first_floor, (void *)NULL);
+  pthread_create(&tid[0], NULL, (void *)ground_floor, (void *)NULL);
+  pthread_create(&tid[1], NULL, (void *)first_floor, (void *)NULL);
 
   pthread_join(tid[0], NULL);
 	pthread_join(tid[1], NULL);
-	pthread_join(tid[2], NULL);
 
   return 0;
 }
