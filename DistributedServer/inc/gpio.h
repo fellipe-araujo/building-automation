e6b258a7e5b3b4414f74de0cd888f82b4d61c8de
Fellipe <araujo.fellipe@hotmail.com>
2021-09-30 21:39:39 -0300

    Updated pins values.

--- DistributedServer/inc/gpio.h
+++ DistributedServer/inc/gpio.h
@@ -3,33 +3,59 @@
 
 #include <wiringPi.h>
 
+/*
+  TÉRREO
+    * 04 -> 07 | Lâmpada da Sala 01
+    * 17 -> 0  | Lâmpada da Sala 02
+    * 27 -> 02 | Luz do Corredor
+    * 07 -> 11 | Ar-condicionado
+    * 26 -> 25 | Sensor de Presença
+    * 23 -> 04 | Sensor de Fumaça
+    * 09 -> 13 | Sensor de Janela 01
+    * 11 -> 14 | Sensor de Janela 02
+    * 10 -> 12 | Sensor de Porta
+    * 13 -> 23 | Sensor de Contagem de Entrada
+    * 19 -> 24 | Sensor de Contagem de Saída
+    * 16 -> 27 | Aspersor de Incêndio
+  
+  PRIMEIRO ANDAR
+    * 22 -> 03 | Lâmpada da Sala 101
+    * 25 -> 06 | Lâmpada da Sala 102
+    * 08 -> 10 | Luz do Corredor
+    * 12 -> 26 | Ar-condicionado
+    * 18 -> 01 | Sensor de Presença
+    * 24 -> 05 | Sensor de Fumaça
+    * 05 -> 21 | Sensor de Janela 101
+    * 04 -> 07 | Sensor de Janela 102
+*/
+
 // Ground Floor
-#define LS_T01 4
-#define LS_T02 17
-#define LC_T 27
-#define AC_T 7
-#define SP_T 26
-#define SF_T 23
-#define SJ_T01 9
-#define SJ_T02 11
-#define SPo_T 10
-#define SC_IN 13
-#define SC_OUT 19
-#define ASP 16
+#define LS_T01 7
+#define LS_T02 0
+#define LC_T 2
+#define AC_T 11
+#define SP_T 25
+#define SF_T 4
+#define SJ_T01 13
+#define SJ_T02 14
+#define SPo_T 12
+#define SC_IN 23
+#define SC_OUT 24
+#define ASP 27
 
 // First Floor
-#define LS_101 22
-#define LS_102 25
-#define LC_1 8
-#define AC_1 12
-#define SP_1 18
-#define SF_1 24
-#define SJ_101 5
-#define SJ_102 4
+#define LS_101 3
+#define LS_102 6
+#define LC_1 10
+#define AC_1 26
+#define SP_1 1
+#define SF_1 5
+#define SJ_101 21
+#define SJ_102 7
 
 void gpio_setup();
 int device_exist(int device);
-void gpio_handler(int floor);
+void* gpio_handler();
 void change_device_state(int device, int state);
 void disable_output_devices();
 
